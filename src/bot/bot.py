432d6ac6d58058f23d4b0d1636002bbe05527e49
Alexey <delvinru@gmail.com>
2021-04-22 14:02:22 +0300

    Setup commands for Telegram interface

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -4,14 +4,27 @@ import sys
 from loguru import logger
 
 from util.handlers import *
-from util.settings import dp
+from aiogram import Bot
+from aiogram.types import BotCommand
+from util.settings import dp, bot
 import xlparser.parser as parser
 
 
+async def set_commands(bot: Bot):
+    """ Register command for Telegram interface """
+
+    commands = [
+        BotCommand(command='/start', description='Начать взаимодействие с ботом'),
+        BotCommand(command='/today', description='Узнать расписание на сегодня'),
+        BotCommand(command='/next', description='Узнать расписание на завтра'),
+        BotCommand(command='/week', description='Узнать расписание на неделю')
+    ]
+    await bot.set_my_commands(commands)
 
 async def main():
     """ Start long polling """
-
+    # Setup bot commands
+    await set_commands(bot)
     # Start long pollings for bot
     await dp.start_polling()
 
